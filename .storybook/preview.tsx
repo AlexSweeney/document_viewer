40360c31ca2f4fc49ef3b4cc64813a1ba9ea18e3
Alex Sweeney <a.sweeney@ryte.com>
2026-07-16 19:13:16 +0100

    install data fetching deps - react query

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,15 +1,19 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
+import { QueryClientProvider } from "@tanstack/react-query";
 import type { Preview } from "@storybook/react-vite";
+import { queryClient } from "../src/queryClient";
 import { theme } from "../src/theme";
 
 const preview: Preview = {
   decorators: [
     (Story) => (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Story />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Story />
+        </ThemeProvider>
+      </QueryClientProvider>
     ),
   ],
   parameters: {
