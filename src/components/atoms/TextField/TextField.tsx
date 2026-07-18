47dfa0289eca03112287f0687821fbf662713566
Alex Sweeney <a.sweeney@ryte.com>
2026-07-18 10:27:52 +0100

    add value prop to TextField

--- src/components/atoms/TextField/TextField.tsx
+++ src/components/atoms/TextField/TextField.tsx
@@ -4,18 +4,21 @@ import { getTextFieldStyles } from "./TextField.styles";
 
 type TextFieldProps = {
   label: string;
+  value?: string;
   size?: TextFieldSize;
   onChange?: (value: string) => void;
 };
 
 export const TextField = ({
   label,
+  value,
   size = "medium",
   onChange,
 }: TextFieldProps) => {
   return (
     <MuiTextField
       label={label}
+      value={value}
       style={getTextFieldStyles(size)}
       onChange={(event) => onChange?.(event.target.value)}
     />
